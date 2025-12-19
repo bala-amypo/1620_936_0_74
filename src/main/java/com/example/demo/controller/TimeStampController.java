c2d8e3469ca378c71e423ecec5bbaf7bfe3f14b0
bala-amypo <bala@amypo.in>
2025-12-19 08:53:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/TimeStampController.java
+++ src/main/java/com/example/demo/controller/TimeStampController.java
@@ -11,7 +11,7 @@ import com.example.demo.service.TimeStampService;
 import com.example.demo.entity.TimeStampEntity;
 import jakarta.validation.Valid;
 @RestController
-public class Controller{
+public class TimeStampController{
     @Autowired TimeStampService serve;
     
     @PostMapping("/postTS")                                                                                                  
