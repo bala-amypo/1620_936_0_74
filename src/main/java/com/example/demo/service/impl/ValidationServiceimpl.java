7a2f4550529772380f922f8bcc301400eb707654
bala-amypo <bala@amypo.in>
2025-12-19 04:53:21 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
@@ -17,6 +17,6 @@ public class ValidationServiceimpl implements ValidationService{
 
     @Override
     public ValidationEntity getData(int id){
-        return student.findById(id).orElseThrow();
+        return student.findById(id).orElseThrow()-> new validationException("Inv")
     }
 }
